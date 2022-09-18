61e7cee0b082b7f51edbb5c6bfd6d90f8415e8e1
Timmer <Timmer@DESKTOP-PLTADMM>
2022-09-19 00:18:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p17._1
+{
+    internal class Program
+    {
+        static int factorial(int i)
+        {
+            int res;
+
+            if (i == 1)
+            {
+                return 1;
+            }
+            res = factorial(i - 1) * i;
+            return res;
+        }
+
+        static void Main()
+        {
+            int n;
+            Console.Write("n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"res = {factorial(n)}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("p17.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("p17.1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c0515317-90f3-4e5f-ae42-b81b4a04043b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
